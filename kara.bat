f4b8b3edbef7d8a87c9201e8aae05750b1d7e731
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-03-11 23:29:55 +0100

    simplify startable scripts

--- kara.bat
+++ kara.bat
@@ -5,9 +5,6 @@ set kara_home=%~dp0
 
 set jars=%kara_home%lib\*.jar
 
-set cp_jars=%kara_home%out\jars\KaraLib.jar;%kara_home%out\jars\KaraExec.jar
-
-
 for %%j in (%jars%) do (
    set cp_jars=%%j;!cp_jars!
 )
@@ -15,4 +12,3 @@ for %%j in (%jars%) do (
 set cp_jars=!cp_jars!
 
 java -DKARA_HOME=%kara_home% -classpath %cp_jars% kara.KaraPackage %*
-
