01eeedf0127ed04f6044fbb487da4e1a9157e339
Maxim Shafirov <max@jetbrains.com>
2013-05-18 21:44:29 -0700

    Sample .war deployment

--- src/KaraLib/src/kara/app/Servlet.kt
+++ src/KaraLib/src/kara/app/Servlet.kt
@@ -3,10 +3,27 @@ package kara.internal
 import javax.servlet.http.*
 import java.io.IOException
 import kara.*
+import javax.servlet.ServletConfig
+import kara.app.AppLoader
 
 open class Servlet() : HttpServlet() {
-    val app: Application = run {
-        throw RuntimeException("Not implemented yet!")
+    private var _app: Application? = null
+
+    val app: Application get() {
+        return _app ?: run {
+            _app = loadApp()
+            _app!!
+        }
+    }
+
+    private fun loadApp(): Application {
+        val config:AppConfig = AppConfig() {
+            javaClass.getClassLoader()!!.getResource("config/$it")
+        }
+
+        val loader = AppLoader(config)
+        loader.loadApp()
+        return loader.application!!
     }
 
     override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
