12d6dad89fd21037a343f6de93e52ef9ae68bb04
Ilya Ryzhenkov <orangy@jetbrains.com>
2013-09-02 18:49:03 +0400

    Unify actions and buttons

--- src/Kootstrap/src/Controls.kt
+++ src/Kootstrap/src/Controls.kt
@@ -9,9 +9,12 @@ public fun HtmlBodyTag.blockAction(h: highlight = highlight.default, c: StyleCla
 public fun HtmlBodyTag.action(size: caliber, c: StyleClass? = null, body: A.()->Unit): Unit = a(s("btn") + (if (size.value.length() > 0) s("btn-${size.value}") else null) + c, contents = body)
 public fun HtmlBodyTag.action(h: highlight = highlight.default, size: caliber = caliber.default, c: StyleClass? = null, body: A.()->Unit): Unit = action(size, s("btn-${h.name()}") + c, body)
 public fun HtmlBodyTag.action(url: Link, h: highlight = highlight.default, size: caliber = caliber.default, c: StyleClass? = null, body: A.()->Unit): Unit = action(h, size, c) { href = url; body() }
+
+public fun HtmlBodyTag.button(size: caliber, c: StyleClass? = null, body: BUTTON.()->Unit): Unit = button(s("btn") + (if (size.value.length() > 0) s("btn-${size.value}") else null) + c, contents = body)
+public fun HtmlBodyTag.button(h: highlight = highlight.default, size: caliber = caliber.default, c: StyleClass? = null, body: BUTTON.()->Unit): Unit = button(size, s("btn-${h.name()}") + c, body)
+public fun HtmlBodyTag.button(url: Link, h: highlight = highlight.default, size: caliber = caliber.default, c: StyleClass? = null, body: BUTTON.()->Unit): Unit = button(h, size, c) { href = url; body() }
+
 public fun HtmlBodyTag.actionGroup(c: StyleClass? = null, body: HtmlBodyTagWithText.()->Unit): Unit = p(c) { body() }
-public fun HtmlBodyTag.button(h: highlight, body: BUTTON.()->Unit): Unit = button(s("btn btn-${h.name()}"), contents = body)
-public fun HtmlBodyTag.buttonSmall(h: highlight, body: BUTTON.()->Unit): Unit = button(s("btn btn-sm btn-${h.name()}"), contents = body)
 public fun HtmlBodyTag.blockButton(h: highlight, body: BUTTON.()->Unit): Unit = button(s("btn btn-block btn-${h.name()}"), contents = body)
 
 public fun HtmlBodyTag.controlWithIcon(iconName: String, body: DIV.()->Unit): Unit = div(s("input-group")) { span(s("input-group-addon")) { icon(iconName) }; body() }
