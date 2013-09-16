c6011c7e3fb13abc5c9f875dfd9bc0a0023754f3
Ilya Ryzhenkov <orangy@jetbrains.com>
2013-09-16 14:44:35 +0400

    Add optional styles to Text, e.g. pull-right

--- src/Kootstrap/src/Text.kt
+++ src/Kootstrap/src/Text.kt
@@ -19,7 +19,7 @@ public fun HtmlBodyTag.panelBody(body: DIV.()->Unit) : Unit = div(s("panel-body"
 
 public fun HtmlBodyTag.jumbo(body: DIV.()->Unit) : Unit = div(s("jumbotron"), contents = body)
 
-public fun HtmlBodyTag.muted(body: SPAN.()->Unit) : Unit = span(s("text-muted"), contents = body)
-public fun HtmlBodyTag.mutedSmall(body: SMALL.()->Unit) : Unit = small(s("text-muted"), contents = body)
+public fun HtmlBodyTag.muted(c : StyleClass? = null, body: SPAN.()->Unit) : Unit = span(s("text-muted")+c, contents = body)
+public fun HtmlBodyTag.mutedSmall(c : StyleClass? = null, body: SMALL.()->Unit) : Unit = small(s("text-muted")+c, contents = body)
 
 public fun HtmlBodyTag.icon(name : String, c : StyleClass? = null) : Unit = i(s("icon-$name") + c)
