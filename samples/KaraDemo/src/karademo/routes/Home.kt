4800aa28f216a9ddac002901ef01f209c0880379
Ilya Ryzhenkov <orangy@jetbrains.com>
2013-09-18 15:53:06 +0400

    Update json request

--- samples/KaraDemo/src/karademo/routes/Home.kt
+++ samples/KaraDemo/src/karademo/routes/Home.kt
@@ -7,22 +7,22 @@ import karademo.models.Book
 
 object Home {
     Get("/")
-    class Index(): Request({ karademo.views.home.Index() })
+    class Index() : Request({ karademo.views.home.Index() })
 
     Get("/test")
-    class Test(): Request({
+    class Test() : Request({
         TextResult("This is a test action, yo")
     })
 
     Post("/updatebook")
-    class Update(): Request({
+    class Update() : Request({
         println("parameters:")
         println(params.getHash("book").toString())
         redirect("/forms")
     })
 
     Get("/forms")
-    class Forms(): Request({
+    class Forms() : Request({
         val book = Book("Ender's Game") {
             description = "This is an excellent book about a boy who gets drawn into an interstellar war."
             isPublished = true
@@ -32,28 +32,30 @@ object Home {
     })
 
     Get("/json")
-    class JsonPage: Request({
-        jsonObject {
-            jsonValue("version", 5)
-            jsonObject("people")
-            {
-                jsonValue("id", 1)
-                jsonValue("name", "Ilya")
-            }
-            jsonArray("cities")
-            {
-                jsonValue("a")
-                jsonValue("b")
-                jsonValue("c")
-                jsonObject {
-                    jsonValue("x", "y")
+    class JsonPage : Request({
+        json {
+            jsonObject {
+                jsonValue("version", 5)
+                jsonObject("people")
+                {
+                    jsonValue("id", 1)
+                    jsonValue("name", "Ilya")
+                }
+                jsonArray("cities")
+                {
+                    jsonValue("a")
+                    jsonValue("b")
+                    jsonValue("c")
+                    jsonObject {
+                        jsonValue("x", "y")
+                    }
                 }
             }
         }
     })
 
     object Posts {
-        Get("get/:id") class GetPost(id: Int): Request({
+        Get("get/:id") class GetPost(id: Int) : Request({
             val post = karademo.models.Post(Date(), "Post ${id}")
             post.body = "This is the <em>post</em> body"
 
