a3f7ae6f952eaf814269915235aee84a3b5f7c76
Ilya Ryzhenkov <orangy@jetbrains.com>
2013-09-23 22:23:28 +0400

    Add bootstrap javascript to resources

--- src/Kootstrap/src/kotlin/html/bootstrap/Resources.kt
+++ src/Kootstrap/src/kotlin/html/bootstrap/Resources.kt
@@ -8,6 +8,12 @@ class SausaJS() : EmbeddedResource("text/javascript", "content/sausa.js") {}
 Get("/static/scripts/sausa.min.js")
 class SausaMinJS() : EmbeddedResource("text/javascript", "content/sausa.min.js") {}
 
+Get("/static/scripts/bootstrap.js")
+class BootstrapJS() : EmbeddedResource("text/javascript", "content/bootstrap.js") {}
+
+Get("/static/scripts/bootstrap.min.js")
+class BootstrapMinJS() : EmbeddedResource("text/javascript", "content/bootstrap.min.js") {}
+
 Get("/static/css/bootstrap.css")
 class Bootstrap() : EmbeddedResource("text/css", "content/bootstrap.css") {}
 
