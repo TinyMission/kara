0e91753d21365cf5a221cd143a355f2a2912c90c
Ilya Ryzhenkov <orangy@jetbrains.com>
2013-09-26 14:26:24 +0400

    Make kootstrap resources singletons so that caching actually works

--- src/Kootstrap/src/kotlin/html/bootstrap/Resources.kt
+++ src/Kootstrap/src/kotlin/html/bootstrap/Resources.kt
@@ -3,19 +3,19 @@ package kotlin.html.bootstrap
 import kara.*
 
 Get("/static/scripts/sausa.js")
-class SausaJS() : EmbeddedResource("text/javascript", "content/sausa.js") {}
+object SausaJS : EmbeddedResource("text/javascript", "content/sausa.js") {}
 
 Get("/static/scripts/sausa.min.js")
-class SausaMinJS() : EmbeddedResource("text/javascript", "content/sausa.min.js") {}
+object SausaMinJS : EmbeddedResource("text/javascript", "content/sausa.min.js") {}
 
 Get("/static/scripts/bootstrap.js")
-class BootstrapJS() : EmbeddedResource("text/javascript", "content/bootstrap.js") {}
+object BootstrapJS : EmbeddedResource("text/javascript", "content/bootstrap.js") {}
 
 Get("/static/scripts/bootstrap.min.js")
-class BootstrapMinJS() : EmbeddedResource("text/javascript", "content/bootstrap.min.js") {}
+object BootstrapMinJS : EmbeddedResource("text/javascript", "content/bootstrap.min.js") {}
 
 Get("/static/css/bootstrap.css")
-class Bootstrap() : EmbeddedResource("text/css", "content/bootstrap.css") {}
+object Bootstrap : EmbeddedResource("text/css", "content/bootstrap.css") {}
 
 Get("/static/css/bootstrap.min.css")
-class BootstrapMin() : EmbeddedResource("text/css", "content/bootstrap.min.css") {}
+object BootstrapMin : EmbeddedResource("text/css", "content/bootstrap.min.css") {}
