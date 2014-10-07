ac3c29b87d9f06ba39807b801f34e018c275cae7
gorohoroh <gorohoroh@gmail.com>
2014-10-07 20:20:57 +0400

    Removing invalid tab characters from JSON strings + tests.

---
+++ src/KaraTests/src/kara.tests/json/JsonString.kt
@@ -0,0 +1,24 @@
+import kotlin.test.*
+import kara.*
+import org.apache.log4j.*
+import org.junit.*
+
+class JsonValidatorTest() {
+    Before fun setUp() {
+        BasicConfigurator.configure()
+    }
+
+    Test fun ensureNoTabsInJson1() {
+        val inputStringWithTab = "емнадцати лет;?	акт органа опек"
+        val outputJsonWithoutTab = jsonString(inputStringWithTab)
+        assertEquals("емнадцати лет;?    акт органа опек", outputJsonWithoutTab.value)
+    }
+
+    Test fun ensureNoTabsInJson2() {
+        val inputStringWithTabs = "Actually these types of questions could be solved using different kinds of pre- and post release researches. We can offer the next options:\n-	Usability testing\n-	A/B testing"
+        val outputJsonWithoutTab = jsonString(inputStringWithTabs)
+        assertEquals("Actually these types of questions could be solved using different kinds of pre- and post release researches. We can offer the next options:\\n-    Usability testing\\n-    A/B testing", outputJsonWithoutTab.value)
+    }
+
+
+}
